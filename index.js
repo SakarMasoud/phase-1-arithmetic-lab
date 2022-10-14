4e3f67f556e173897613d71bfe41764f43593e75
Sakar Masoud <masoudsakar@gmail.com>
2022-10-14 14:22:26 +0300

    Sakar Masoud

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = (Math.random()*10 + 1) ;
+random = Number.parseInt(random, 10);
+
+let num3 = 34;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,20);
